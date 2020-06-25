3822cb3c3d652b609e45193a693a0b3a3551f41e
Hyunse Kim <anzmf12@naver.com>
2020-06-24 23:29:26 -0400

    sql

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express'),
+  sql = require('mssql'),
+  router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  getPerson();
+  res.send('success');
 });
+
+function getPerson() {
+  var config = {
+    user: '',
+    password: '',
+    server: '', // it can be servername if connecting directly with sqlserver or can be replace by RDS end point
+    database: '',
+  };
+
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    if (err) {
+      console.log(err);
+
+      return;
+    }
+
+    request.query(
+      `select * from Person.Person where FirstName = 'Ken'`,
+      function (err, recordset) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(recordset);
+        }
+
+        sql.close();
+      }
+    );
+  });
+}
 
 module.exports = router;
